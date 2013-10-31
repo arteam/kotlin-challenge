57436406af1f55b4f823d9f8189d3b60687b0bb0
ArTeam <arteamon@gmail.com>
2013-11-01 01:08:53 +0400

    Task F

--- src/F.kt
+++ src/F.kt
@@ -1,5 +1,4 @@
 import java.io.BufferedReader
-import java.util.ArrayList
 
 fun main(args: Array<String>) {
     val reader = BufferedReader(System.`in`.reader("UTF-8"))
@@ -12,38 +11,33 @@ fun floyd(input: BufferedReader): String {
     var m = firstLine[1]
     val cap = Array(n) { Array(n) { 0 } }
     while (m-- > 0){
-        val row = input.readLine()!!.split(" ").map { it -> it.toInt() }
-        cap[row[0] - 1][row[1] - 1] = row[2]
+        val row = input.readLine()!!.split(" ")
+        cap[row[0].toInt()-1][row[1].toInt()-1]=row[2].toInt()
     }
 
     val from = 0;
-    val to = n - 1;
-    var flow = 0
-    while (true){
-        if(from == to) return "0"
-        val innerFlow = findPath(from, to, BooleanArray(n), Integer.MAX_VALUE, cap)
-        if(innerFlow == 0 || innerFlow==Integer.MAX_VALUE) return flow.toString()
-        flow += innerFlow
-    }
+    val to = n-1;
 
-}
-
-
-fun findPath(a: Int, b: Int, visited: BooleanArray, flow: Int, cap: Array<Array<Int>>): Int {
-    if(a == b)
-      return flow
-    visited[a] = true
-    for (i in (0..visited.size - 1)) {
-        if(cap[a][i] > 0 && !visited[i]){
-            val innerFlow = findPath(i, b, visited, Math.min(flow, cap[a][i]), cap)
-            if(innerFlow > 0 ){
-                cap[a][i] -= innerFlow
-                cap[i][a] += innerFlow
-                return innerFlow
+    fun findPath(a: Int, b: Int, visited: BooleanArray, flow: Int): Int {
+        if(a == b) return flow
+        visited[a] = true
+        for (i in (0..n-1)) {
+            if(cap[a][i] > 0 && !visited[i]){
+                val innerFlow = findPath(i, b, visited, Math.min(flow, cap[a][i]))
+                if(innerFlow > 0){
+                    cap[a][i] -= innerFlow
+                    cap[i][a] += innerFlow
+                    return innerFlow
+                }
             }
         }
+        return 0
     }
-    return 0
+
+    var flow = 0
+    while (true){
+        val innerFlow = findPath(from, to, BooleanArray(n), Integer.MAX_VALUE)
+        if(innerFlow == 0) return flow.toString()
+        flow += innerFlow
+    }
 }
-
-
